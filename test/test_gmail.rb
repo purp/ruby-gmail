ad3d1436d022ac859e6e8e74f9f755f447aa4360
Jim Meyer <jim@geekdaily.org>
2010-11-30 22:42:32 -0800

    That's a fairly complete set of tests for Gmail::Mailbox

--- test/test_gmail.rb
+++ test/test_gmail.rb
@@ -75,10 +75,64 @@ class GmailMailboxTest < Test::Unit::TestCase
   def setup
     @gmail = setup_gmail_mock
     @mailbox = Gmail::Mailbox.new(@gmail, 'I know my name')
+    @imap_connection.stubs(:select).with(@mailbox.name)
   end
   
   def test_mailbox_knows_its_name
     assert_equal @mailbox.name, 'I know my name'
     assert_equal @mailbox.to_s, @mailbox.name
+  end
+  
+  def test_mailbox_has_default_empty_message_hash
+    assert_equal @mailbox.messages, {}
+  end
+  
+  def test_mailbox_has_proper_count
+    @imap_connection.expects(:uid_search).once.returns([])
+    assert_equal 0, @mailbox.count
+    
+    @imap_connection.expects(:uid_search).once.returns(%w(Sam Cassie Ellie))
+    assert_equal 3, @mailbox.count
+  end
+  
+  def test_mailbox_emails_search_defaults_to_all
+    @imap_connection.expects(:uid_search).with(['ALL']).once.returns([])
+    assert_equal [], @mailbox.emails
+  end
+  
+  def test_mailbox_emails_handles_aliases
+    @imap_connection.expects(:uid_search).with(['ALL']).once.returns([])
+    assert_equal [], @mailbox.emails(:all)
+    
+    @imap_connection.expects(:uid_search).with(['SEEN']).once.returns([])
+    assert_equal [], @mailbox.emails(:read)
+    
+    @imap_connection.expects(:uid_search).with(['UNSEEN']).once.returns([])
+    assert_equal [], @mailbox.emails(:unread)
+  end
+  
+  def test_mailbox_emails_accepts_direct_array_of_search_options
+    search_opts = %w(Beth Sam Cassie Ellie)
+    @imap_connection.expects(:uid_search).with(search_opts).once.returns([])
+    assert_equal [], @mailbox.emails(search_opts)
+  end
+  
+  def test_mailbox_emails_translates_options_to_search_params_properly
+    right_now = Time.now
+    
+    @imap_connection.expects(:uid_search).with(['ALL', 'SINCE', right_now.to_imap_date]).once.returns([])
+    assert_equal [], @mailbox.emails(:after => right_now)
+    
+    @imap_connection.expects(:uid_search).with(['ALL', 'BEFORE', right_now.to_imap_date]).once.returns([])
+    assert_equal [], @mailbox.emails(:before => right_now)
+    
+    @imap_connection.expects(:uid_search).with(['ALL', 'ON', right_now.to_imap_date]).once.returns([])
+    assert_equal [], @mailbox.emails(:on => right_now)
+    
+    @imap_connection.expects(:uid_search).with(['ALL', 'FROM', 'Me']).once.returns([])
+    assert_equal [], @mailbox.emails(:from => 'Me')
+    
+    @imap_connection.expects(:uid_search).with(['ALL', 'TO', 'You']).once.returns([])
+    assert_equal [], @mailbox.emails(:to => 'You')
   end
 end
