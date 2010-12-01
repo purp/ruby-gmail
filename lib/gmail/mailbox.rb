26fd9b8a30eeed476f3822998bebad60188aefdf
Jim Meyer <jim@geekdaily.org>
2010-11-30 22:41:55 -0800

    Make a few notes on things that need doing and stop apologizing for making semantic coding possible

--- lib/gmail/mailbox.rb
+++ lib/gmail/mailbox.rb
@@ -11,6 +11,7 @@ class Gmail
     attr_reader :name
 
     def initialize(gmail, name)
+      ### FIXME: call it parent or something and quit touching @gmail directly
       @gmail = gmail
       @name = name
     end
@@ -31,6 +32,7 @@ class Gmail
         search = ['ALL']
         opts = key_or_opts
       elsif key_or_opts.is_a?(Symbol) && opts.is_a?(Hash)
+        ### fixme: this should be a constant
         aliases = {
           :all => ['ALL'],
           :unread => ['UNSEEN'],
@@ -58,13 +60,12 @@ class Gmail
       end
     end
 
-    # This is a convenience method that really probably shouldn't need to exist, but it does make code more readable
-    # if seriously all you want is the count of messages.
     def count(*args)
       emails(*args).length
     end
 
     def messages
+      ### FIXME: Hash.new {|h, k| h[k] = Message.new(@gmail, self, k)}
       @messages ||= {}
     end
   end
